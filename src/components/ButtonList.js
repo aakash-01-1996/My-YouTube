7233d1c3bb4a4d206edfad1ac04e80c585205668
aakash <aakashambodkar99@gmail.com>
2024-02-28 21:15:06 -0500

    Half Way YT

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Gaming" />
+      <Button name="Songs" />
+      <Button name="Cricket" />
+      <Button name="Live" />
+      <Button name="Soccer" />
+      <Button name="News" />
+      <Button name="Valentines" />
+      <Button name="Cricket" />
+      <Button name="Live" />
+      <Button name="Soccer" />
+      <Button name="News" />
+      <Button name="Valentines" />
+      <Button name="News" />
+      <Button name="Valentines" />
+    </div>
+  );
 };
 
 export default ButtonList;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+
   return (
     <div className="p-5 shadow-lg  w-44 ">
       <ul>
